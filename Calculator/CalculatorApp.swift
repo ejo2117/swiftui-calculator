92a2cda49f3a222581b5be5a98f3abb8dba8917b
Ethan <ejo2117@columbia.edu>
2023-10-06 19:18:43 -0400

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Ethan O'Neal on 10/6/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
