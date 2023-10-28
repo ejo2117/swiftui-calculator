9d89c70fec639d96376010657fafc28ffaeeec97
Ethan <ejo2117@columbia.edu>
2023-10-28 16:12:59 -0400

    Set up basic "add" functionality

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -8,12 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var result: Int = 0;
+    @State var leftOperand: Int? = 0;
+    @State var rightOperand: Int? = nil;
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Result: \(result)")
+            Text("\(leftOperand ?? 0 )")
+            Grid {
+                GridRow {
+                    ForEach(7..<10) {number in
+                        Button("\(number)") {
+                            rightOperand = number
+                        }
+                    }
+                }
+                GridRow {
+                    ForEach(4..<7) {number in
+                        Button("\(number)") {
+                            rightOperand = number
+                        }
+                    }
+                }
+                GridRow {
+                    ForEach(1..<4) {number in
+                        Button("\(number)") {
+                            rightOperand = number
+                        }
+                    }
+                }
+            }
+            Button("Clear") {
+                result = 0;
+                leftOperand = 0;
+                rightOperand = 0;
+            }
+            Button("Add"){
+                leftOperand = rightOperand;
+                rightOperand = nil;
+            }
+            Button("Equals"){
+                // TODO: handle "nil" better?
+                if(rightOperand == nil){
+                    return;
+                }
+                // right is always the most recent thing added
+                result = rightOperand! + leftOperand!;
+                leftOperand = result;
+            }
+            // Calc stuff here
+            
+            
+            
+            
         }
         .padding()
     }
