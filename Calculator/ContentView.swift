fb2f9949d2b2b8158497c403b484974c873b2c4e
Ethan <ejo2117@columbia.edu>
2023-10-28 18:06:17 -0400

    work on input display logic

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -8,16 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum OperatorType {
+        case add, subtract
+    }
+    
     @State var result: Int = 0;
     @State var leftOperand: Int? = 0;
     @State var rightOperand: Int? = nil;
     @State var input: String = "";
+    @State var operatorType: OperatorType? = nil;
+    
+    
+    // value to display
+        // either current input or result
+    
+    
     
     var body: some View {
         VStack {
-            Text("Result: \(result)")
-//            Text("\(leftOperand ?? 0 )")
-            Text("\( input.count == 0 ? "0" : input )")
+//            Text("Result: \(result)")
+            // "Display" Logic
+                // If "inputting" (input has length ?) display input value
+                // Else Display "result"
+            Text("\( input.count == 0 ? String(result) : String(Int(input)!) )")
             Grid {
                 GridRow {
                     ForEach(7..<10) {number in
@@ -43,6 +56,12 @@ struct ContentView: View {
                         }
                     }
                 }
+                GridRow {
+                    Button("0") {
+                        input += "0"
+                        rightOperand = Int(input)
+                    }
+                }
             }
             Button("Clear") {
                 result = 0;
@@ -50,20 +69,31 @@ struct ContentView: View {
                 rightOperand = 0;
                 input = "";
             }
+            // Really clicking any operator should just signal that we're in a "entering" state
+            // Need to store the operator value
             Button("Add"){
-                
+                // "Operator Clicked" Logic
+                    // If "operand" is not nil, add(?) current input to result
+                    // Value of current input is stored as operand
+                    
+                    
                 leftOperand = rightOperand;
+                result = leftOperand ?? 0;
                 input = "";
-                rightOperand = nil;
+//                rightOperand = nil;
             }
             Button("Equals"){
+                
+                // "Execute" Logic
+                
                 // TODO: handle "nil" better?
-                if(rightOperand == nil){
-                    return;
-                }
+//                if(rightOperand == nil){
+//                    return;
+//                }
                 // right is always the most recent thing added
-                result = rightOperand! + leftOperand!;
+                result = (rightOperand ?? 0) + (leftOperand ?? 0);
                 leftOperand = result;
+                input = ""
             }
         }
         .padding()
